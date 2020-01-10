8fc89efc576f5ad5a15e5ab2a30b6249e957b555
marisdanne <maris.danne@gmail.com>
2020-01-10 11:47:32 +0200

    Izveidots template

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return "Sveiciens no StartIT!"
+  return "Sveiciens no StartIT!!!"
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
