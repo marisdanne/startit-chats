1c13acd61236cd582ea20ef765ffcb4d9f6bfe91
marisdanne <maris.danne@gmail.com>
2020-01-10 12:16:48 +0200

    pievienots health checks

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask('app')
 def index_page():
   return render_template('index.html')
 
+@app.route('/health')
+def health_check():
+  return "OK"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
